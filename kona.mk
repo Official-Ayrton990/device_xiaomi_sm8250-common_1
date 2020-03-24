4c3c7065a83935e535cf1b1e9240584c8a53b345
trpsl <trpsl755@gmail.com>
2020-03-24 14:10:26 +0100

    sm8250-common: Add Moto Dolby
    Values taken from OP9 Pro and modified by Sasha (@helenius147 on Telegram)
    to work with Moto Dolby.
    
    Co-authored-by: blankaf <realblankaf@gmail.com>
    Co-authored-by: MB <mb150996@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kona.mk
+++ kona.mk
@@ -110,6 +110,14 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libvolumelistener
 
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.audio.dolby.ds2.enabled=true\
+vendor.audio.dolby.ds2.hardbypass=false\
+persist.vendor.audio_fx.current=dolby\
+ro.vendor.dolby.dax.version=DS1_2.2.0.0_r1
+
+
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
@@ -128,6 +136,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    device/xiaomi/sm8250-common/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -339,6 +348,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librmnetctl \
     libxml2
+
+#RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # Rootdir
 PRODUCT_PACKAGES += \
