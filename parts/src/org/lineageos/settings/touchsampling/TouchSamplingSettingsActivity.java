Descendant-Devices/device_xiaomi_sm8250-common

sm8250-common: Initialize High Touch Polling Rate support
used to handle our node to force enable Xiaomi's High Touch polling rate mode from kernel

parts/src/org/lineageos/settings/touchsampling/TouchSamplingSettingsActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.touchsampling;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class TouchSamplingSettingsActivity extends PreferenceActivity {
+
+    private static final String TAG_HTSR = "touchsampling";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new TouchSamplingSettingsFragment(), TAG_HTSR).commit();
+    }
+}

parts/src/org/lineageos/settings/touchsampling/TouchSamplingSettingsFragment.java
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.touchsampling;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import org.lineageos.settings.R;
+import org.lineageos.settings.touchsampling.TouchSamplingUtils;
+import org.lineageos.settings.utils.FileUtils;
+
+public class TouchSamplingSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String HTSR_ENABLE_KEY = "htsr_enable";
+    public static final String SHAREDHTSR = "SHAREDHTSR";
+
+    private SwitchPreference mHTSRPreference;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+      addPreferencesFromResource(R.xml.htsr_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        mHTSRPreference = (SwitchPreference) findPreference(HTSR_ENABLE_KEY);
+        mHTSRPreference.setEnabled(true);
+        mHTSRPreference.setOnPreferenceChangeListener(this);
+        enableHTSR(0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (HTSR_ENABLE_KEY.equals(preference.getKey())) {
+            enableHTSR((Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+    private void enableHTSR(Integer enable) {
+            FileUtils.writeLine(TouchSamplingUtils.HTSR_FILE, enable.toString());
+            SharedPreferences preferences = getActivity().getSharedPreferences(SHAREDHTSR,Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt(SHAREDHTSR, enable);
+            editor.commit();
+    }
+}

parts/src/org/lineageos/settings/touchsampling/TouchSamplingUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.touchsampling;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.utils.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public final class TouchSamplingUtils {
+
+    private static final String TAG = "TouchSamplingUtils";
+    public static final String HTSR_FILE = "/sys/devices/virtual/touch/touch_dev/bump_sample_rate";
+
+    public static void restoreSamplingValue(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(TouchSamplingSettingsFragment.SHAREDHTSR, Context.MODE_PRIVATE);
+        Integer htsrstate = sharedPref.getInt(TouchSamplingSettingsFragment.SHAREDHTSR, 0);
+        FileUtils.writeLine(HTSR_FILE, htsrstate.toString());
+    }
+}

Official-Ayrton990/device_xiaomi_sm8250-common_1

Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

parts/src/org/lineageos/settings/touchsampling/TouchSamplingSettingsActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.aospextended.settings.touchsampling;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class TouchSamplingSettingsActivity extends PreferenceActivity {
+
+    private static final String TAG_HTSR = "touchsampling";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new TouchSamplingSettingsFragment(), TAG_HTSR).commit();
+    }
+}

parts/src/org/lineageos/settings/touchsampling/TouchSamplingSettingsFragment.java
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.aospextended.settings.touchsampling;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import org.aospextended.settings.R;
+import org.aospextended.settings.touchsampling.TouchSamplingUtils;
+import org.aospextended.settings.utils.FileUtils;
+
+public class TouchSamplingSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String HTSR_ENABLE_KEY = "htsr_enable";
+    public static final String SHAREDHTSR = "SHAREDHTSR";
+
+    private SwitchPreference mHTSRPreference;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+      addPreferencesFromResource(R.xml.htsr_settings);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        mHTSRPreference = (SwitchPreference) findPreference(HTSR_ENABLE_KEY);
+        mHTSRPreference.setEnabled(true);
+        mHTSRPreference.setOnPreferenceChangeListener(this);
+        enableHTSR(0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (HTSR_ENABLE_KEY.equals(preference.getKey())) {
+            enableHTSR((Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+    private void enableHTSR(Integer enable) {
+            FileUtils.writeLine(TouchSamplingUtils.HTSR_FILE, enable.toString());
+            SharedPreferences preferences = getActivity().getSharedPreferences(SHAREDHTSR,Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt(SHAREDHTSR, enable);
+            editor.commit();
+    }
+}

parts/src/org/lineageos/settings/touchsampling/TouchSamplingUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.aospextended.settings.touchsampling;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+import androidx.preference.PreferenceManager;
+
+import org.aospextended.settings.utils.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public final class TouchSamplingUtils {
+
+    private static final String TAG = "TouchSamplingUtils";
+    public static final String HTSR_FILE = "/sys/devices/virtual/touch/touch_dev/bump_sample_rate";
+
+    public static void restoreSamplingValue(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(TouchSamplingSettingsFragment.SHAREDHTSR, Context.MODE_PRIVATE);
+        Integer htsrstate = sharedPref.getInt(TouchSamplingSettingsFragment.SHAREDHTSR, 0);
+        FileUtils.writeLine(HTSR_FILE, htsrstate.toString());
+    }
+}